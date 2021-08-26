bc2d159416661780d63a7519bf431944f32b0eee
Sebastian Hancock <sebastian.hancock@tum.de>
2021-08-26 14:34:52 +0200

    remove stuff

--- script.js
+++ script.js
@@ -97,42 +97,6 @@ const projects = [
         })
     }
 
-//const priv_key = '0e8fe66ebe7d63b8a37d8b6533fdcff2121f746c';
-//const pub_key = '9e656fd191967a9bf10b3f6cc034b0ac';
-
-//callMarvel();
-/*i need this for another project, 
-seems cool right? who knew marvel had an API */
-function callMarvel() {
-
-  var ts = new Date().getTime();
-  var hash = CryptoJS.MD5(ts + priv_key + pub_key).toString(); 
-
-  var characterId = '1009718'; // wolverine  
-  //var url = 'http://gateway.marvel.com:80/v1/public/comics';
-
-  var url = 'http://gateway.marvel.com:80/v1/public/events';
-  console.log(url);
-  
-
-  $.getJSON(url, {
-    ts: ts,
-    apikey: pub_key,
-    hash: hash,
-    characters: characterId
-    })
-    .done(function(data) {
-      // sort of a long dump you will need to sort through
-      console.log(data);
-    })
-    .fail(function(err){
-      // the error codes are listed on the dev site
-      console.log(err);
-    });
-
-
-
-}
 
 
 var mapHover = document.getElementById('maps')
@@ -177,3 +141,14 @@ beachHover.addEventListener('mouseout', function(evt) {
   beachpic[0].classList.add('hide');
 
 })
+
+function getLocaltime(){
+  return new Date().getHours();
+}
+console.log(getLocaltime());
+
+if(getLocaltime() < 5 || getLocaltime() > 19) {
+  console.log('good evening')
+} else {
+  console.log('good day')
+}
